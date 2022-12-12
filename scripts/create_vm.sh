cb527524662cd8a11f1e30c0c43ecc5e61dacc03
Alexandre Herssens <alexandre.herssens.etu@univ-lille.fr>
2022-12-12 15:48:58 +0100

    script creation + demarrage OK

--- scripts/create_vm.sh
+++ scripts/create_vm.sh
@@ -2,9 +2,31 @@
 
 VMIUT=/home/public/vm/bin/vmiut
 
-NAME=$0
-IP=$1
+NAME=$1
+IP=$2
 
-$VMIUT creer $NAME
-$VMIUT start $NAME
-$VMIUT info $NAME
+vm_create(){
+	$VMIUT creer $NAME > /dev/null 2>&1
+}
+
+vm_start(){
+	$VMIUT start $NAME > /dev/null 2>&1
+}
+
+dhcp_ok(){
+	CURR_IP=$(vm_ip)
+	if [ "$CURR_IP" = "" ];
+		then sleep 5;  dhcp_ok
+		else return 0;
+	fi
+}
+
+vm_ip(){
+	echo $($VMIUT info $NAME | grep "ip-possible" | cut -d "=" -f 2)
+}
+
+vm_create
+vm_start
+dhcp_ok
+echo Machine virtuelle $NAME créée et démarrée!
+
